ec000b1954b561a2fc363f9a10abfda4d831c491
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-05-26 12:48:09 +0300

    Delete target.cpp

--- target.cpp
+++
@@ -1,8 +0,0 @@
-#include "Target.h"
-
-Target::Target(float x, float y) {
-    shape.setRadius(TARGET_SIZE / 2);
-    shape.setOrigin(TARGET_SIZE / 2, TARGET_SIZE / 2);
-    shape.setPosition(x, y);
-    shape.setFillColor(sf::Color::Red);
-}
