ea55b7b32fd9b67c2ee4769d3dc86497e004dfaf
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-05-26 12:47:11 +0300

    Delete Utilities.h

--- Utilities.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "Mirror.h"
-#include <vector>
-
-float randomFloat(float min, float max);
-bool isTooClose(const std::vector<Mirror>& mirrors, sf::Vector2f point, float minDistance);
